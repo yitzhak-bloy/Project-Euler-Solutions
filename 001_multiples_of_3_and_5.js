17b15e369cd40cd1aed07489d0210096214d7f50
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2020-02-08 21:27:27 +0200

    pix

--- 001_multiples_of_3_and_5.js
+++ 001_multiples_of_3_and_5.js
@@ -1,17 +1,22 @@
-const xxxxx = (num) => {
+/*
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below the provided parameter value number.
+*/
 
-  let numbers = [];
+const multiplesOf3and5 = (num) => {
 
-  for (i = 0; i < num; i ++) {
+  let numbersMultiplesOf3and5 = [];
+
+  // Review all numbers below the given number
+  for (let i = 0; i < num; i ++) {
+
+  // Check for any number if it is multiples of 3 or 5
     if (Number.isInteger(i/3) || Number.isInteger(i/5)) {
-      numbers.push(i)
+      numbersMultiplesOf3and5.push(i)
     }
   }
-  console.log(numbers);
 
-  const sumOfNumbers = numbers.reduce((sum, number) => sum + number);
+  const sumOfNumbers = numbersMultiplesOf3and5.reduce((sum, number) => sum + number);
 
-
-return sumOfNumbers
-
+  return sumOfNumbers
 } 
