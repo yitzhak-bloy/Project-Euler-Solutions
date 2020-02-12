1376ceb66decefef70838a4765567dd90fd35e82
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2020-02-12 19:56:58 +0200

    004 Largest palindrome product

---
+++ 004 Largest palindrome product.js
@@ -0,0 +1,34 @@
+/*
+A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+Find the largest palindrome made from the product of two 3-digit numbers.
+*/
+const largestPalindromeProduct = (n) => {
+
+  let asas = []
+
+  const check = (num) => {
+    // console.log(num)
+    const numAtString = num.toString();
+    const numericInReverse = numAtString.split('').reverse().join('')
+    return numAtString == numericInReverse
+  }
+
+  for (let i = 999; i > 0; i--) {
+    for (let x = 999; x >= i; x--) {
+
+      asas.push({i: i, x: x, ix: i*x})
+      // console.log('i', i, 'x', x, 'i*x', i*x)
+      if (check(i * x)) {
+        console.log(asas)
+
+        // console.log(i*x, 'הצלחנו')
+        return i * x
+      }
+    }
+  }
+
+
+
+}
+
+largestPalindromeProduct(3);
