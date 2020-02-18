1c9f246e2ca6cbf2189964ddd90cdad7e8377887
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2020-02-18 10:28:25 +0200

    008 Largest product in a series

--- 006 Sum square difference.js
+++ 006 Sum square difference.js
@@ -1,29 +1,35 @@
+/*
+The sum of the squares of the first ten natural numbers is,
+12+22+...+102=385
+The square of the sum of the first ten natural numbers is,
+(1+2+...+10)2=552=3025
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025−385=2640.
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+*/
+
 function sumSquareDifference(n) {
+  let allNumbers = [];
 
-  let allNumbers = []
-
+  // The insertion of all numbers up to n into one array
   for (let i = 1; i <= n; i++) {
     allNumbers.push(i)
   }
-
-  const sum = allNumbers.reduce((last, corent) => {
-    return last + corent
+  
+  // Multiplying each number by itself and connecting all results
+  const sumSquares = allNumbers.reduce((lastResult, currentValue) => {
+    return lastResult + currentValue * currentValue;
   })
 
-  const squareSum = sum * sum
-
-  console.log(allNumbers)
-  console.log(squareSum)
-
-
-  const sum of the squares = allNumbers.reduce((last, corent) => {
-    return last + corent * corent
+  // Connecting all numbers up to n
+  const sumOfNumbers = allNumbers.reduce((lastResult, currentValue) => {
+    return lastResult + currentValue;
   })
 
-  return squareSum - sum1
+  // Multiply sumOfNumbers by itself
+  const squareSum = sumOfNumbers * sumOfNumbers;
 
-
-  }
+  // Returns the subtraction of sumSquares from squareSum
+  return squareSum - sumSquares;
+}
   
-  sumSquareDifference(100);
-  
+sumSquareDifference(100);
--- 007 10001st prime.js
+++ 007 10001st prime.js
@@ -1,65 +1,33 @@
-function nthPrime(n) {
-  let num = [2];
+/*
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+What is the 10001st prime number?
+*/
 
-  const check1 = (number) => {
-    for (let x = 0; x < num.length; x++) {
-      if (number % num[x] === 0) {
+function nthPrime(n) {
+  let PrimeNumbersList = [2];
+
+  // Checks whether the number is prime
+  const checkIfPrime = (number) => {
+    for (let x = 0; x < PrimeNumbersList.length; x++) {
+      if (number % PrimeNumbersList[x] === 0) {
         return false
       } 
     }
     return true
   }
 
-  for (let i = 3; i < 100000000; i++) {
-    if (check1(i)) {
-      num.push(i)
+  // Goes over all the numbers and puts them to the test 
+  for (let i = 3; i < n*n; i++) {
+    if (checkIfPrime(i)) {
+      PrimeNumbersList.push(i)
     }
-    if (num.length === n) {
-      return num[num.length - 1]
-    }
-  }
-}
-  
-nthPrime(10001);  
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function nthPrime(n) {
-  let num = [2];
-  let check = true;
-
-  for (let i = 3; i < 1000000; i++) {
-    check = true;
-    for (let x = 0; x < num.length; x++) {
-      if (i % num[x] === 0) {
-        check = false
-      } 
-    }
-    if (check) {
-      num.push(i)
-    }
-    if (num.length === n) {
-      return num[num.length - 1]
+    // When it comes to n prime numbers returns the last number
+    if (PrimeNumbersList.length === n) {
+      return PrimeNumbersList[PrimeNumbersList.length - 1]
     }
   }
+
 }
   
 nthPrime(10001);  
