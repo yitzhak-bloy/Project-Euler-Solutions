181daa1531dd4c6fdc93fd6a25c866d861fd008b
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2020-03-13 15:42:25 +0200

    Solution to problem 013 - with poor performance, needs improvement.

--- 014 Longest Collatz sequence.js
+++ 014 Longest Collatz sequence.js
@@ -1,45 +1,41 @@
 function longestCollatzSequence(limit) {
+  let array = [];
+  let object = {};
 
-  let array = []
-
-  for (let i = 0; i < limit; i++) {
+  for (let i = limit; i > 0; i--) {
     array.push(i)
-    
   }
-
 
   const check = (num) => {
-    let arr = [num, ]
+    let arr = []
     let z = num;
+
     while (z > 1) {
-    //  console.log(z);
-
       if (z % 2 === 0) {
+        arr.push(z)
+        if (array.indexOf(z) > -1) {
+          delete array[array.indexOf(z)];
+        }
         z = z/2
-        arr.push(z)
       } else {
-        z = z*3+1
-        arr.push(z)
-      }
-     z--;
-     console.log(arr.length)
-     return arr
+          if (array.indexOf(z) > -1) {
+            delete array[array.indexOf(z)];
+          }
+          arr.push(z)
+          z = z*3+1
+        }
     }
-    
-
+    arr.push(1)
+    object[num] = arr.length
   }
-  for (let x = 0; x < array.length; x++) {
-    
-    if (check(x)) {
-      // console.log(x)
+
+  array.map((num) => {
+    if (num) {
+      check(num)
     }
-    
-  }
+  });
 
+  return Number(Object.keys(object).reduce((a, b) => object[a] > object[b] ? a : b));
+} 
 
-    // Good luck!
-    // return true;
-  }
-  
-  longestCollatzSequence(14);
-  
+longestCollatzSequence(5847);
