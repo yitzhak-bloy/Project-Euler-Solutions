846044306884fa864ffb4ef50cef3453584addc4
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2020-03-28 23:59:29 +0300

    018 Maximum path sum I Add comments

--- 018 Maximum path sum I.js
+++ 018 Maximum path sum I.js
@@ -1,28 +1,53 @@
+/*
+By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
+
+3
+7 4
+2 4 6
+8 5 9 3
+
+That is, 3 + 7 + 4 + 9 = 23.
+
+Find the maximum total from top to bottom of the triangle below:
+
+75
+95 64
+17 47 82
+18 35 87 10
+20 04 82 47 65
+19 01 23 75 03 34
+88 02 77 73 07 63 67
+99 65 04 28 06 16 70 92
+41 41 26 56 83 40 80 70 33
+41 48 72 33 47 32 37 16 94 29
+53 71 44 65 25 43 91 52 97 51 14
+70 11 33 28 77 73 17 78 39 68 17 57
+91 71 52 38 17 14 91 43 58 50 27 29 48
+63 66 04 68 89 53 67 30 73 16 69 87 40 31
+04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
+
+NOTE: As there are only 16384 routes, it is possible to solve this problem by trying every route. However, Problem 67, is the same challenge with a triangle containing one-hundred rows; it cannot be solved by brute force, and requires a clever method! ;o)
+*/
+
 const maximumPathSumI = (triangle) => {
-  let triangle1 = triangle;
 
   const chack = (down, up, i) => {
     for (let x = 0; x < up.length; x++) {
       if (up[x] + down[x] > up[x] + down[x+1]) {
-        triangle1[i-2][x] = up[x] + down[x];
+        triangle[i-2][x] = up[x] + down[x];
       } else {
-        triangle1[i-2][x] = up[x] + down[x+1];
+        triangle[i-2][x] = up[x] + down[x+1];
       }
     }
   }
 
-  for (let i = triangle1.length; i > 0; i--) {
-    if (triangle1[i-2]) {
-      chack(triangle1[i-1], triangle1[i-2], i)    
+  for (let i = triangle.length; i > 0; i--) {
+    if (triangle[i-2]) {
+      chack(triangle[i-1], triangle[i-2], i)    
     }
   }
-  return triangle1[0][0]
+  return triangle[0][0]
 }
-  
-const testTriangle = [[3, 0, 0, 0],
-                      [7, 4, 0, 0],
-                      [2, 4, 6, 0],
-                      [8, 5, 9, 3]];
 
 const numTriangle = [[75, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                     [95, 64, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -40,4 +65,4 @@ const numTriangle = [[75, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                     [63, 66, 4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31, 0],
                     [4, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 4, 23]];
 
-maximumPathSumI(testTriangle);  
+maximumPathSumI(numTriangle);  
