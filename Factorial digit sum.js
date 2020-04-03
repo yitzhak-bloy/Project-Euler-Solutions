547a8e84e68a6bb8a83c6147168e3dc41912adca
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2020-04-03 11:16:20 +0300

    Factorial digit sum

---
+++ Factorial digit sum.js
@@ -0,0 +1,23 @@
+/*
+n! means n × (n − 1) × ... × 3 × 2 × 1
+
+For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+Find the sum of the digits in the number 100!
+*/
+
+function sumFactorialDigits(n) {
+  let sumOfFactorial = 1;
+
+  // Calculate the factorial of the given number
+  for (let i = n; i > 0; i--) {
+    sumOfFactorial = BigInt(sumOfFactorial) * BigInt(i)
+  }
+
+  // Returns the sum of the Factorial digits
+  return sumOfFactorial.toString().split('').reduce((sum, number) => Number(sum) + Number(number))
+
+}
+  
+sumFactorialDigits(100);
